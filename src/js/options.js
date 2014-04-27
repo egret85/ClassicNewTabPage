6eb2d1ccf619417ee978ea50f52e6d8b44875657
Everette Beatley <e_beatley@hotmail.com>
2014-04-27 01:55:09 -0400

    Updated Option page to allow selecting, saving, and clearing the background image.

--- src/js/options.js
+++ src/js/options.js
@@ -1,34 +1,98 @@
+/**
+ * Load current settings from localStorage
+ */
+var backgroundSrc = localStorage.getItem('cntp.op.background');
+
+if (backgroundSrc)
+{
+    setBackgroundImage(backgroundSrc);
+}
 
 
 /**
- * Save it all
+ * Save settings to localStorage
  */
-$('.submit').click(function (ev) {
+$('#saveButton').click(function (ev)
+{
 
-  ev.preventDefault();
+    ev.preventDefault();
 
-  // Save the inputs based on their values
-  $(inputs).each(function () {
+    //save background setting
+    var val = "";
 
-    var val = $(this).val(),
-        key = 'cntp.op.' + $(this).attr('name'),
-        placeholder = $(this).attr('placeholder');
-
-    if( val !== placeholder ) {
-      if( val === '' ) val = placeholder;
-      localStorage.setItem(key, val);
+    if ($('#backgroundPreviewImg').length)
+    {
+        val = $('#backgroundPreviewImg')[0].src;
     }
 
-  });
+    localStorage.setItem('cntp.op.background', val);
 
-  // Indicate to the user that things went well
-  $(this).text('Saved').addClass("saved");
-
+    // Indicate to the user that things went well
+    $(this).text('Saved').addClass("saved");
 });
 
 /**
  * Reset the save button
  */
-$('input').on('keyup click', function () {
-  $('.submit').text('Save').removeClass("saved");
+$('input').on('keyup click', function ()
+{
+    $('#saveButton').text('Save').removeClass("saved");
 });
+
+
+/**
+ * Handle background select
+ */
+
+$('#backgroundInput').on('change', function ()
+{
+    var file = $('#backgroundInput')[0].files[0];
+    var imageType = /image.*/;
+
+    if (file.type.match(imageType))
+    {
+        var reader = new FileReader();
+
+        reader.onload = function ()
+        {
+            setBackgroundImage(reader.result);
+        }
+
+        reader.readAsDataURL(file);
+    }
+    else
+    {
+        $('#backgroundError')[0].innerHTML = "File not supported!"
+
+        $('#backgroundError').show();
+    }
+
+});
+
+$('#clearButton').click(function ()
+{
+    $('#backgroundCurrent').hide();
+    $('#backgroundPreviewImg').remove();
+    $('#backgroundInput')[0].value = "";
+    $('#backgroundInput').show();
+    $('#saveButton').text('Save').removeClass("saved");
+});
+
+
+function setBackgroundImage(imageSrc)
+{
+    $('#backgroundPreview')[0].innerHTML = "";
+
+    var img = new Image();
+    img.id = "backgroundPreviewImg";
+    img.src = imageSrc;
+    img.width = 211;
+    img.height = 119;
+
+
+    $('#backgroundPreview')[0].appendChild(img);
+
+    $('#backgroundError').hide();
+    $('#backgroundInput').hide();
+    $('#backgroundCurrent').show();
+}
