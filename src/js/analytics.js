0b7a62a02430faf64599ae51f384548e1b276015
Everette Beatley <e_beatley@hotmail.com>
2014-08-16 21:02:54 -0400

    Cleaned up some junk code.

--- src/js/analytics.js
+++ src/js/analytics.js
@@ -1,8 +1,8 @@
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-48596656-3']);
 _gaq.push(['_trackPageview']);
-_gaq.push('_setCustomVar',1,'Name','@name@', 2);
-_gaq.push('_setCustomVar',2,'Version','@version@', 2);
+_gaq.push('_setCustomVar',1,'Name','@name@');
+_gaq.push('_setCustomVar',2,'Version','@version@');
 
 (function() {
     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
